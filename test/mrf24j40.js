de498662c51c137259235b154178e11da69ec03c
Michele Balistreri <michele@briksoftware.com>
2014-04-14 17:25:53 +0200

    implemented low-level read/write methods

--- test/mrf24j40.js
+++ test/mrf24j40.js
@@ -32,7 +32,7 @@ describe('MRF24J40', function() {
       radio.readControlRegister(ControlRegister["PACON2"], function(value) {
         value.should.equal(0xCA);
         done();
-      })
+      });
     });
   });
   
@@ -50,7 +50,7 @@ describe('MRF24J40', function() {
       radio.readControlRegister(ControlRegister["REMCNTL"], function(value) {
         value.should.equal(0xAC);
         done();
-      })
+      });
     });
   });
   
@@ -59,14 +59,14 @@ describe('MRF24J40', function() {
       var radio = new MRF24J40(hw);
       
       hw.setTransferCallback(function(txBuf, rxBuf, cb) {
-        txBuf.should.deep.equal(new Buffer([0x25, 0xfa]));
+        txBuf.should.deep.equal(new Buffer([0x25, 0xFA]));
         rxBuf.length.should.equal(2);
         cb();
       });
       
-      radio.writeControlRegister(ControlRegister["ACKTMOUT"], 0xfa, function(value) {
+      radio.writeControlRegister(ControlRegister["ACKTMOUT"], 0xFA, function(value) {
         done();
-      })
+      });
     });
   });
   
@@ -75,12 +75,12 @@ describe('MRF24J40', function() {
       var radio = new MRF24J40(hw);
       
       hw.setTransferCallback(function(txBuf, rxBuf, cb) {
-        txBuf.should.deep.equal(new Buffer([0x25, 0xfa]));
+        txBuf.should.deep.equal(new Buffer([0x25, 0xFA]));
         rxBuf.length.should.equal(2);
         cb();
       });
       
-      radio.writeControlRegister(ControlRegister["ACKTMOUT"], 0xfa, function(value) {
+      radio.writeControlRegister(ControlRegister["ACKTMOUT"], 0xFA, function(value) {
         done();
       })
     });
@@ -89,14 +89,84 @@ describe('MRF24J40', function() {
       var radio = new MRF24J40(hw);
       
       hw.setTransferCallback(function(txBuf, rxBuf, cb) {
-        txBuf.should.deep.equal(new Buffer([0xC5, 0xf0, 0xaf]));
+        txBuf.should.deep.equal(new Buffer([0xC5, 0xF0, 0xAF]));
         rxBuf.length.should.equal(3);
         cb();
       });
       
-      radio.writeControlRegister(ControlRegister["TESTMODE"], 0xaf, function(value) {
+      radio.writeControlRegister(ControlRegister["TESTMODE"], 0xAF, function(value) {
         done();
-      })
+      });
+    });
+  });
+  
+  describe('#readFIFO', function() {
+    it('should read multiple bytes from a FIFO from offset 0', function(done) {
+      var radio = new MRF24J40(hw);
+      
+      hw.setTransferCallback(function(txBuf, rxBuf, cb) {
+        txBuf.should.deep.equal(new Buffer([0xE0, 0x00, 0x00, 0x00]));
+        rxBuf.length.should.equal(4);
+        rxBuf[2] = 0xCA;
+        rxBuf[3] = 0xFE;
+        
+        cb(rxBuf);
+      });
+      
+      radio.readFIFO(FIFO["RX"], 0, 2, function(data) {
+        data.should.deep.equal(new Buffer([0xCA, 0xFE]));
+        done();
+      });
+    });
+    
+    it('should read multiple bytes from a FIFO from non-zero offset', function(done) {
+      var radio = new MRF24J40(hw);
+      
+      hw.setTransferCallback(function(txBuf, rxBuf, cb) {
+        txBuf.should.deep.equal(new Buffer([0xE0, 0x20, 0x00, 0x00]));
+        rxBuf.length.should.equal(4);
+        rxBuf[2] = 0xCA;
+        rxBuf[3] = 0xFE;
+        
+        cb(rxBuf);
+      });
+      
+      radio.readFIFO(FIFO["RX"], 1, 2, function(data) {
+        data.should.deep.equal(new Buffer([0xCA, 0xFE]));
+        done();
+      });
+    });
+  });
+
+  describe('#writeFIFO', function() {
+    it('should write multiple bytes from a FIFO at offset 0', function(done) {
+      var radio = new MRF24J40(hw);
+      
+      hw.setTransferCallback(function(txBuf, rxBuf, cb) {
+        txBuf.should.deep.equal(new Buffer([0x80, 0x10, 0xCA, 0xFE]));
+        rxBuf.length.should.equal(4);
+        
+        cb();
+      });
+      
+      radio.writeFIFO(FIFO["TX"], 0, new Buffer([0xCA, 0xFE]), function(data) {
+        done();
+      });
+    });
+    
+    it('should write multiple bytes from a FIFO at non-zero offset', function(done) {
+      var radio = new MRF24J40(hw);
+      
+      hw.setTransferCallback(function(txBuf, rxBuf, cb) {
+        txBuf.should.deep.equal(new Buffer([0x90, 0x30, 0xCA, 0xFE]));
+        rxBuf.length.should.equal(4);
+        
+        cb();
+      });
+      
+      radio.writeFIFO(FIFO["TXBEACON"], 1, new Buffer([0xCA, 0xFE]), function(data) {
+        done();
+      });
     });
   });
 });
